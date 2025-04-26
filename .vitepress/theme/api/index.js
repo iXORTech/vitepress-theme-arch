1535ea8dec224309489a871c9b8aec208bcad60d
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2025-04-25 23:28:26 -0400

    feat: Implement i18n in api.
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- .vitepress/theme/api/index.js
+++ .vitepress/theme/api/index.js
@@ -1,24 +1,25 @@
+import { useI18n } from '@/utils/i18n'
+
 /**
- * 获取给定网址的站点图标和描述
- * @param {string} url - 站点 URL
+ * Get Info for Given Website
+ * @param {string} url - Website URL
  * @returns {Promise<{iconUrl: string, description: string}>}
  */
 export const getSiteInfo = async (url) => {
+  const { i18n } = useI18n()
+
   const details = {
     iconUrl: null,
     title: null,
     description: null,
   };
   try {
-    // 站点数据
     const response = await fetch(url);
     const text = await response.text();
     const parser = new DOMParser();
     const doc = parser.parseFromString(text, "text/html");
-    // 获取页面标题
     const titleElement = doc.querySelector("title");
-    details.title = titleElement ? titleElement.textContent : "暂无标题";
-    // 获取 icon
+    details.title = titleElement ? titleElement.textContent : i18n('api.get-site-info.no-title');
     let iconLink =
       doc.querySelector("link[rel='shortcut icon']") || doc.querySelector("link[rel='icon']");
     if (iconLink) {
@@ -26,39 +27,10 @@ export const getSiteInfo = async (url) => {
     } else {
       details.iconUrl = new URL("/favicon.ico", url).href;
     }
-    // 获取描述
     const metaDescription = doc.querySelector("meta[name='description']");
-    details.description = metaDescription ? metaDescription.content : "暂无站点描述";
+    details.description = metaDescription ? metaDescription.content : i18n('api.get-site-info.no-description');
   } catch (error) {
-    console.error("获取站点信息失败：", error);
+    console.error("Get Website Info Error: ", error);
   }
   return details;
-};
-
-/**
- * 站点统计数据
- */
-export const getStatistics = async (key) => {
-  const result = await fetch(`https://v6-widget.51.la/v6/${key}/quote.js`);
-  const title = [
-    "最近活跃",
-    "今日人数",
-    "今日访问",
-    "昨日人数",
-    "昨日访问",
-    "本月访问",
-    "总访问量",
-  ];
-  const data = await result.text();
-  let num = data.match(/(<\/span><span>).*?(\/span><\/p>)/g);
-  num = num.map((el) => {
-    const val = el.replace(/(<\/span><span>)/g, "");
-    return val.replace(/(<\/span><\/p>)/g, "");
-  });
-  const statistics = {};
-  for (let i = 0; i < num.length; i++) {
-    if (i === num.length - 1) continue;
-    statistics[title[i]] = num[i];
-  }
-  return statistics;
 };
