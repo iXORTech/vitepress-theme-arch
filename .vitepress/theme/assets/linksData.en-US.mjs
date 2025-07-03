3ee01868c4957a3abe0b3e9091f97c427e826c41
Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>
2025-07-03 10:59:54 -0400

    feat: Add localized links data file.
    Signed-off-by: Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>

---
+++ .vitepress/theme/assets/linksData.en-US.mjs
@@ -0,0 +1,20 @@
+// Friend Links & Recommended Sites
+
+const linksData = [
+  {
+    entries: [
+      {
+        avatar: "https://img.qubik.top/CubikAvatar/56728b-outline.PNG",
+        description: "This is a sample of friend links.",
+        screenshot: "",
+        title: "Sample Link",
+        url: "https://qubik.top"
+      }
+    ],
+    group: "friends",
+    groupDesc: "My Friends",
+    groupName: "Friends"
+  },
+];
+
+export default linksData;
---
+++ .vitepress/theme/assets/linksData.zh-CN.mjs
@@ -0,0 +1,20 @@
+// Friend Links & Recommended Sites
+
+const linksData = [
+  {
+    entries: [
+      {
+        avatar: "https://img.qubik.top/CubikAvatar/56728b-outline.PNG",
+        description: "这是一个示例链接。",
+        screenshot: "",
+        title: "示例链接",
+        url: "https://qubik.top"
+      }
+    ],
+    group: "friends",
+    groupDesc: "我的朋友们",
+    groupName: "朋友们"
+  },
+];
+
+export default linksData;
